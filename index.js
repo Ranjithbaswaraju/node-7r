3ebfd57ebab8b02925ee04a8bb8a8c8dc05776f5
Ranjithbaswaraj <ranjithbaswaraju10@gmail.com>
2024-11-15 07:43:01 +0530

    day-2task

--- index.js
+++ index.js
@@ -1,75 +1,45 @@
-
-
-const http=require('http')
+const http=require("http")
 const port=3200
+const data=["nodejs"]
+
+const userDetails={u_id:258,username:"john",email:"john@123"};
+const userDataArray=[userDetails,userDetails,userDetails,userDetails]
 
 const server=http.createServer((req,res)=>{
-    
-    // res.writeHead(200,{"content-type":"text/html"})
-    // res.writeHead(200,{"content-type":"text/xml"})
-    res.writeHead(404,{"content-type":"application/json"})
-    res.write("Hello")
+
+    res.writeHead(200,{"content-type":"application/json"})
+    res.write(
+        JSON.stringify({statusMessage:"user details",data:userDataArray}))
     res.end()
 })
 
-// server.listen(port,()=>{
-//     console.log(`server is runnning at ${port}`)
-// })
+server.listen(port,()=>{
+    console.log("server is running")
+})
 
-//To convet the in data form of different data types:
-
-// const http=require('http')
-// const port=3200
-// const data=["nodejs"]
-// const server=http.createServer((req,res)=>{
-    
-
-//     res.writeHead(200,{"content-type":"application/json"})
-//     res.write(JSON.stringify(data))
-//     res.end()
-// })
-
-// server.listen(port,()=>{
-//     console.log("server is running")
-// })
-//["nodejs"]
-
-//{data:["nodejs"]}
-
-// const http=require("http")
-// const port=3200
-// const data=["nodejs"]
-
-// const server=http.createServer((req,res)=>{
-
-//     res.writeHead(200,{"content-type":"application/json"})
-//     res.write(
-//         JSON.stringify({statusMessage:"data retrived succesfully",data:data}))
-//     res.end()
-// })
-
-// server.listen(port,()=>{
-//     console.log("server is running")
-// })
-
-
-// const http=require("http")
-// const port=3200
-// const data=["nodejs"]
-
-// const userDetails={u_id:258,username:"john",email:"john@123"};
-
-
-// const server=http.createServer((req,res)=>{
-
-//     res.writeHead(200,{"content-type":"application/json"})
-//     res.write(
-//         JSON.stringify({statusMessage:"user details",data:userDetails}))
-//     res.end()
-// })
-
-// server.listen(port,()=>{
-//     console.log("server is running")
-// })
-
-
+// OUTPUT:
+// {
+//     "statusMessage": "user details",
+//     "data": [
+//       {
+//         "u_id": 258,
+//         "username": "john",
+//         "email": "john@123"
+//       },
+//       {
+//         "u_id": 258,
+//         "username": "john",
+//         "email": "john@123"
+//       },
+//       {
+//         "u_id": 258,
+//         "username": "john",
+//         "email": "john@123"
+//       },
+//       {
+//         "u_id": 258,
+//         "username": "john",
+//         "email": "john@123"
+//       }
+//     ]
+//   }
