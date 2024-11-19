c9fa7ba7080232ca2a9fefc1c75f6872038d1692
Ranjithbaswaraj <ranjithbaswaraju10@gmail.com>
2024-11-19 06:16:09 +0530

    updated

--- index.js
+++ index.js
@@ -2,10 +2,35 @@
 // require('./file2')
 // require('./file3')
 // console.log("hello world11")
-// common js modules
+// // common js modules
 //  var imp=require('./file1')
 //  console.log(imp)
-//  console.log(imp.a,imp.b)
+// //  console.log(imp.a,imp.b)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 // importing through destructrinbg
@@ -13,25 +38,31 @@
 // console.log(a,b,c)
 
 // ES6 modules
+// import {a,b,c} from './file1.js'
+// console.log(a,b,c)
 
-// import {a,b} from './file1.js'
-// console.log(a,b)
 
-// importing functions in node js
-// const {a,b,c,sample}=require("./file1")
-// sample()
+
+
+
+
+
+
+importing functions in node js
+const {a,b,c,sample}=require("./file1")
+sample()
 
 
 // server creation
 
-const http=require("http");
-const port=3100
-const server=http.createServer((req,res)=>{
+// const http=require("http");
+// const port=3100
+// const server=http.createServer((req,res)=>{
 
-    res.write("Welcome to node");
-    res.end("hello")
-})
+//     res.write("Welcome to node");
+//     res.end("hello")
+// })
 
-server.listen(port,()=>{
-    console.log(`server is running at ${port}`)
-})
+// server.listen(port,()=>{
+//     console.log(`server is running at ${port}`)
+// })
